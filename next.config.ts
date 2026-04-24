a44278dfb306c2c24a56b581606fa8f73cddec05
Serhii Kulyk <sergey.kulyk.dev@gmail.com>
2026-04-24 13:40:28 +0300

    fix: add image quality 85 and allow external API in CSP connect-src

--- next.config.ts
+++ next.config.ts
@@ -25,7 +25,7 @@ const nextConfig: NextConfig = {
               "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
               "font-src 'self' https://fonts.gstatic.com",
               "img-src 'self' data: blob: https:",
-              "connect-src 'self'",
+              "connect-src 'self' https://api.my-community.pp.ua",
               "frame-ancestors 'none'",
             ].join("; "),
           },
